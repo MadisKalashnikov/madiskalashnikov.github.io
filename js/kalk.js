87931a6c15c18fb0acf1caf16cc6af86fbbb0083
Madis <96111936+MadisKalashnikov@users.noreply.github.com>
2021-12-29 16:32:24 +0200

    added parsefloat to result, fixed decimal to 2

--- js/kalk.js
+++ js/kalk.js
@@ -1,5 +1,5 @@
 function calc() {
     n1 = parseFloat(document.getElementById("n1").value);
     n2 = parseFloat(document.getElementById("n2").value);
-    result = document.getElementById("result").value = n1 * n2 / 1000;
+    result = document.getElementById("result").value = parseFloat(n1 * n2 / 1000).toFixed(2);
 }
