1954bb90844a83d24ee7df28500e5940d9120ec3
Madis <96111936+MadisKalashnikov@users.noreply.github.com>
2021-12-30 09:31:41 +0200

    removed third variable

--- js/kalk.js
+++ js/kalk.js
@@ -1,6 +1,5 @@
 function calc() {
     n1 = parseFloat(document.getElementById("n1").value);
     n2 = parseFloat(document.getElementById("n2").value);
-    n3 = parseFloat(document.getElementById("n3").value);
-    result = document.getElementById("result").value = parseFloat(n1 * n2 * n3 / 1000).toFixed(2);
+    result = document.getElementById("result").value = parseFloat(n1 * n2 / 1000).toFixed(2);
 }
